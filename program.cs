aa5524e00549b683b30b20786a7d82f2dfd83705
John Metzger <56820659+jometzg@users.noreply.github.com>
2024-08-23 13:31:19 +0100

    Update program.cs

--- program.cs
+++ program.cs
@@ -14,6 +14,14 @@ builder.AddAzureOpenAIChatCompletion(
     "ENDPOINT",
     "KEY");
 
+// add logging, if needed
+builder.Services.AddLogging(c => 
+{
+    c.AddConsole();
+    c.SetMinimumLevel(LogLevel.Trace);
+});
+
+// add the lights and microbit plugins
 builder.Plugins.AddFromType<LightsPlugin>();
 builder.Plugins.AddFromType<MicrobitPlugin>();
 Kernel kernel = builder.Build();
@@ -21,15 +29,17 @@ Kernel kernel = builder.Build();
 // Retrieve the chat completion service from the kernel
 IChatCompletionService chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
 
-// 2. Enable automatic function calling
+// Enable automatic function calling
 OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new() 
 {
     ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
 };
 
-// Create the chat history
+// Create the chat history, sort of system prompt (need to check)
 ChatHistory history = new ChatHistory("""
-    You are a friendly assistant who likes to follow the rules. You have a matrix of 5 rows and columns each of which can have a brightness of 9 .
+    You have a matrix of 5 rows and columns each of which can have a brightness of 0 to 9.
+    Rows make up the horizontal axis. The top row has coordinates 0,1 to 4, 0.
+    Columns represent the vertical axis. The left most column is 0,0 to 0, 4 and the right column is 4,0 to 4,4.
     """);
 
 
